624a1731f5454520b812c0c547fb5152646f6e18
meagle21 <emanuel.eagle@gmail.com>
2025-02-23 20:45:15 -0500

    update

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,8 +1,8 @@
-output "api_key" {
-  value     = aws_api_gateway_api_key.main.value
-  sensitive = true
-}
+# output "api_key" {
+#   value     = aws_api_gateway_api_key.main.value
+#   sensitive = true
+# }
 
-output "api_url" {
-  value = "${aws_api_gateway_stage.main.invoke_url}/items"
-}
+# output "api_url" {
+#   value = "${aws_api_gateway_stage.main.invoke_url}/items"
+# }
