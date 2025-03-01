e6a94e4da8652244aad0ed193310d52b2fa7e905
meagle21 <emanuel.eagle@gmail.com>
2025-03-01 14:10:20 -0500

    update

--- deployment_scripts/generate_qrcode.py
+++ deployment_scripts/generate_qrcode.py
@@ -1,7 +1,27 @@
-import qrcode
 import boto3
+import io
+import qrcode
+import os
+
+QR_CODES_S3_BUCKET_NAME = os.environ["QR_CODES_S3_BUCKET_NAME"]
+
+print(QR_CODES_S3_BUCKET_NAME)
 
 url = "www.google.com"
 img = qrcode.make(rf"https://{url}")
-print(type(img))
+img_byte_arr = io.BytesIO()
+img.save(img_byte_arr, format='PNG')
+img_byte_arr.seek(0)  # Move to the beginning of BytesIO object
 
+# s3_client = boto3.client('s3',
+#     aws_access_key_id='YOUR_ACCESS_KEY',
+#     aws_secret_access_key='YOUR_SECRET_KEY',
+#     region_name='YOUR_REGION'
+# )
+
+# s3_client.upload_fileobj(
+#     img_byte_arr,
+#     bucket_name,
+#     object_key,
+#     ExtraArgs={'ContentType': 'image/png'}
+# )
