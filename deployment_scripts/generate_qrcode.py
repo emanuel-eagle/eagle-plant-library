3704d33ff0e7bd5cfcc38a08ac34f7878d3c35c6
meagle21 <emanuel.eagle@gmail.com>
2025-03-02 12:04:12 -0500

    udpate

--- deployment_scripts/generate_qrcode.py
+++ deployment_scripts/generate_qrcode.py
@@ -8,7 +8,7 @@ QR_CODES_S3_BUCKET_NAME = os.environ["QR_CODES_S3_BUCKET_NAME"]
 AWS_ACCESS_KEY_ID = os.environ["AWS_ACCESS_KEY_ID"]
 AWS_SECRET_ACCESS_KEY = os.environ["AWS_SECRET_ACCESS_KEY"]
 URL = os.environ["API_GATEWAY_URL"]
-PARAMETERS = os.ENVIRON["PARAMETERS"]
+PARAMETERS = os.environ["PARAMETERS"]
 crafted_url = "?filename="
 
 img = qrcode.make(crafted_url)
