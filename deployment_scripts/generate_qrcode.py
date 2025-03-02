af7ddc86ba5065a8f2281477ea48480f7c479531
meagle21 <emanuel.eagle@gmail.com>
2025-03-02 12:05:56 -0500

    update

--- deployment_scripts/generate_qrcode.py
+++ deployment_scripts/generate_qrcode.py
@@ -9,7 +9,7 @@ AWS_ACCESS_KEY_ID = os.environ["AWS_ACCESS_KEY_ID"]
 AWS_SECRET_ACCESS_KEY = os.environ["AWS_SECRET_ACCESS_KEY"]
 URL = os.environ["API_GATEWAY_URL"]
 PARAMETERS = os.environ["PARAMETERS"]
-crafted_url = "?filename="
+crafted_url = f"{URL}?filename={PARAMETERS}"
 
 img = qrcode.make(crafted_url)
 img_byte_arr = io.BytesIO()
